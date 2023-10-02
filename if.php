054f951b6d2706fb3b7d7a6d385a0119c6122c00
HelloCodeman <955khm@gmail.com>
2023-10-02 11:14:28 +0800

    PHP基礎-switch條件公式

--- if.php
+++ if.php
@@ -2,7 +2,7 @@
 
 $score=75;
 
-echo "我的成績" . $score;
+echo "我的成績:" . $score;
 echo "<br>";
 echo "判斷為:";
 
@@ -10,4 +10,26 @@ if($score>=60){
     echo "及格";
 }else{
     echo "不及格";
+}
+
+$level="B";
+
+echo "成績等級為: .$level";
+echo "<br>";
+
+switch ($level) {
+    case "A":
+        echo "表現優良";
+        break;
+        case "B":
+    echo "值得肯定";
+    break;
+    case "C":
+        echo "需要更多練習";
+        break;
+        case "D":
+            echo "需要加強基本功";
+            break;
+        default:
+        echo "是否無心學業?";
 }
