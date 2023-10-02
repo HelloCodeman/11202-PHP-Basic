d49b5254d6a2fb4a000db8697588565e67d501fe
HelloCodeman <955khm@gmail.com>
2023-10-02 11:38:25 +0800

    PHP基礎-成績判斷練習

--- if.php
+++ if.php
@@ -1,6 +1,6 @@
 <?php
 
-$score=75;
+$score=97;
 
 echo "我的成績:" . $score;
 echo "<br>";
@@ -12,9 +12,25 @@ if($score>=60){
     echo "不及格";
 }
 
-$level="B";
+echo "<br>";
 
-echo "成績等級為: .$level";
+if($score>=90 && $score<=100){
+    $level="A";
+}
+if($score>=80 && $score<=89){
+    $level="B";
+}
+if($score>=70 && $score<=79){
+    $level="C";
+}
+if($score>=60 && $score<=69){
+    $level="D";
+}
+if($score>=0 && $score<=59){
+    $level="E";
+}
+
+echo "成績等級為:" .$level;
 echo "<br>";
 
 switch ($level) {
